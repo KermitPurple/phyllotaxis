307d1f43f2b8cc812c14b9402e3a6382879b93b7
Shane McDonough <shanemcdman@gmail.com>
2020-03-11 00:35:17 -0700

    Change size and make position change with size

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ from numpy import sin, cos, sqrt
 os.environ["SDL_VIDEO_WINDOW_POS"] = "15,30"
 
 pygame.display.init()
-screen = pygame.display.set_mode((600,600))
+size = 650
+screen = pygame.display.set_mode((size,size))
 rotating_angle = 0
 circle_size = 3
 
 def draw_spiral():
-    for n in range(2000):
+    for n in range(2300):
         theta = (n * 137.5 * 3.14159265)/180
         theta += rotating_angle
         r = 10 * sqrt(n)
-        point = (int(r * cos(theta))+300, int(r * sin(theta))+300)
+        point = (int(r * cos(theta))+int(size/2), int(r * sin(theta))+int(size/2))
         color = pygame.Color(255,255,255)
         color.hsva = (n % 360, 100, 100)
         pygame.draw.circle(screen, color, point, circle_size)
