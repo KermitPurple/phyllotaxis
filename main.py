99e9f95a39d0edea48f23b3f189c97c8b1d0f38a
Shane McDonough <shanemcdman@gmail.com>
2020-03-11 00:29:10 -0700

    Make circle size editable during runtime

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ os.environ["SDL_VIDEO_WINDOW_POS"] = "15,30"
 pygame.display.init()
 screen = pygame.display.set_mode((600,600))
 rotating_angle = 0
+circle_size = 3
 
 def draw_spiral():
     for n in range(2000):
@@ -15,13 +16,18 @@ def draw_spiral():
         point = (int(r * cos(theta))+300, int(r * sin(theta))+300)
         color = pygame.Color(255,255,255)
         color.hsva = (n % 360, 100, 100)
-        pygame.draw.circle(screen, color, point, 3)
+        pygame.draw.circle(screen, color, point, circle_size)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.unicode == "+":
+                circle_size += 1
+            elif event.unicode == "-":
+                circle_size -= 1
     screen.fill((0,0,0))
     draw_spiral()
     rotating_angle += 0.01
