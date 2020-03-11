b4c5a9d7a11d75f1e603d7d3aeb8e957c1c97972
Shane McDonough <shanemcdman@gmail.com>
2020-03-10 23:51:32 -0700

    create draw spiral v1

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import pygame
 import os
+from numpy import sin, cos, sqrt
 os.environ["SDL_VIDEO_WINDOW_POS"] = "15,30"
 
 pygame.display.init()
 screen = pygame.display.set_mode((600,600))
 
 def draw_spiral():
-    point = (300,300)
-    pygame.draw.circle(screen, (255,255,255), point, 5)
+    for n in range(2000):
+        theta = (n * 137.5 * 3.14159265)/180
+        r = 10 * sqrt(n)
+        point = (int(r * cos(theta))+300, int(r * sin(theta))+300)
+        pygame.draw.circle(screen, (255,255,255), point, 5)
 
 running = True
 while running:
