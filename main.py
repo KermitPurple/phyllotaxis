3e202e9f5b3975c4880166c9d5bdb6449986fbd0
Shane McDonough <shanemcdman@gmail.com>
2020-03-11 00:00:45 -0700

    Add rotation

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ os.environ["SDL_VIDEO_WINDOW_POS"] = "15,30"
 
 pygame.display.init()
 screen = pygame.display.set_mode((600,600))
+rotating_angle = 0
 
 def draw_spiral():
     for n in range(2000):
         theta = (n * 137.5 * 3.14159265)/180
+        theta += rotating_angle
         r = 10 * sqrt(n)
         point = (int(r * cos(theta))+300, int(r * sin(theta))+300)
         pygame.draw.circle(screen, (255,255,255), point, 5)
@@ -19,4 +21,5 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     draw_spiral()
+    rotating_angle += 0.001
     pygame.display.update()
